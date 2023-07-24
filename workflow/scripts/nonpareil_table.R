0508451eabf7a05a49d96511c41081260aba0338
Jorge Langa <6546145+jlanga@users.noreply.github.com>
2023-07-24 17:04:47 +0200

    pre-commit r code and linting of files

--- workflow/scripts/nonpareil_table.R
+++ workflow/scripts/nonpareil_table.R
@@ -2,14 +2,20 @@
 library(Nonpareil)
 suppressPackageStartupMessages(library(tidyverse))
 
-args = commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 
-if (length(args)!=2) {
-  stop("Input and/or output files are missing", call.=FALSE)
+if (length(args) != 2) {
+  stop("Input and/or output files are missing", call. = FALSE)
 }
 
-Nonpareil.set(args[1], plot=F) %>%
-	summary() %>%
-	as.data.frame() %>%
-	rownames_to_column(., var = "sample") %>%
-	write.table(.,args[2],col.names=T,row.names=F,sep="\t",quote=F)
+Nonpareil.set(args[1], plot = FALSE) %>%
+  summary() %>%
+  as.data.frame() %>%
+  rownames_to_column(., var = "sample") %>%
+  write.table(
+    args[2],
+    col.names = TRUE,
+    row.names = FALSE,
+    sep = "\t",
+    quote = FALSE
+  )
